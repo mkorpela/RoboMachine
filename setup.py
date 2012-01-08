b63ea1fffe25e17f7f44d91bb071abb4f7fee6d2
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-01-08 16:24:43 +0200

    bin --> scripts

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from distutils.core import setup
 import os
 from setuptools import find_packages
 
+name = 'Mikko Korpela'
+# I might be just a little bit too much afraid of those bots..
+address = name.lower().replace(' ', '.')+chr(64)+'gmail.com'
 
 setup(name='RoboMachine',
       version='0.1',
       description='Test data generator for Robot Framework',
-      author='Mikko Korpela',
+      author=name,
+      author_email=address,
+      url='https://github.com/mkorpela/RoboMachine',
       packages=find_packages(),
-      scripts = [os.path.join('bin','robomachine'),
-                 os.path.join('bin','robomachine.bat')],
+      scripts = [os.path.join('scripts','robomachine'),
+                 os.path.join('scripts','robomachine.bat')],
       install_requires = ['pyparsing']
      )
