67b74e619afa9dae506738ddbf193beaa3735f2f
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-01-15 17:42:47 +0200

    tests for generation

---
+++ robomachine/test/generation_test.py
@@ -0,0 +1,61 @@
+from StringIO import StringIO
+import unittest
+from robomachine.model import RoboMachine, State, Action
+import robomachine
+
+
+class GenerationTestCase(unittest.TestCase):
+
+    def test_generation_with_no_tests(self):
+        mock_machine = RoboMachine(states=[], variables=[], rules=[])
+        output = StringIO()
+        robomachine.generate(machine=mock_machine, output=output, strategy=_MockStrategyWithNoTests)
+        self.assertEqual('*** Test Cases ***', output.getvalue())
+
+    def test_generation_with_no_tests(self):
+        mock_machine = RoboMachine(states=[State('bar', [], [])], variables=[], rules=[])
+        output = StringIO()
+        robomachine.generate(machine=mock_machine, max_tests=10, output=output, strategy=_MockStrategyWithHundredTests)
+        self.assertEqual(10, sum(1 for line in output.getvalue().splitlines() if line.startswith('Test ')))
+
+    def test_generation_with_same_generated_tests(self):
+        mock_machine = RoboMachine(states=[State('bar', [], [])], variables=[], rules=[])
+        output = StringIO()
+        robomachine.generate(machine=mock_machine, max_tests=1000, output=output, strategy=_MockStrategyWithSameTests)
+        self.assertEqual(1, sum(1 for line in output.getvalue().splitlines() if line.startswith('Test ')))
+
+
+class _MockStrategy(object):
+
+    def __init__(self, machine, max_actions):
+        self._machine = machine
+        self._max_actions = max_actions
+
+    def _action(self):
+        a = Action('foo', 'bar', None)
+        a.set_machine(self._machine)
+        return a
+
+
+class _MockStrategyWithNoTests(_MockStrategy):
+
+    def tests(self):
+        return []
+
+
+class _MockStrategyWithHundredTests(_MockStrategy):
+
+    def tests(self):
+        return [([self._action()], []) for i in range(100)]
+
+
+class _MockStrategyWithSameTests(_MockStrategy):
+
+    def tests(self):
+        test = ([self._action()], [])
+        while True:
+            yield test
+
+
+if __name__ == '__main__':
+    unittest.main()
