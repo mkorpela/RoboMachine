3bdbc945a463a8ab2a76860b383861fe7a601aab
David Kaplan <david.kaplan@handitover.se>
2017-04-06 00:52:15 +0200

    Updated the example app
     - Keyword library now has better docstrings and decorators;
        change state is now a private method
     - Removed one argument from the robomachine call in test.sh

--- example/webapp/test.sh
+++ example/webapp/test.sh
@@ -6,7 +6,6 @@ INFILE=$BASENAME.robomachine
 OUTFILE=$BASENAME.robot
 
 python -m robomachine.runner --tests-max 20 --actions-max 8 \
-          --to-state 'Login Page' \
           --output $OUTFILE --generate-dot-graph svg \
           --generation-algorithm allpairs-random \
           $INFILE
