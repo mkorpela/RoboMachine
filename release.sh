c21b8658e4e7f7676358eab94d245620a9bb0a87
Mikko Korpela <mikko.korpela@gmail.com>
2022-12-30 17:09:10 +0200

    release.sh update

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@ cd ..
 git clone git@github.com:mkorpela/RoboMachine.git robomachinerelease
 cd robomachinerelease
 python setup.py sdist
-sudo twine upload dist/*.*
+twine upload -r RoboMachine dist/*.*
